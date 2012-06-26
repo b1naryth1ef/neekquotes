845286ffdfaa400d85f511ee532cc2f2bcb99ec3
Andrei Z <b1naryth1ef@gmail.com>
2012-06-26 02:59:35 -0500

    Fixed stuff up. Quotes are now at /

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ for q in os.listdir('./quotes/'):
 	if not q.endswith('.txt'): continue
 	with open('./quotes/'+q) as f:
 		data = [i.strip() for i in f.readlines()]
-	q = {'name':q[:-4].replace('_', ' ').title(), 'len':len(data), 'q':[]}
-	for line in data:
-		q['q'].append(line.split(' ', 1))
+	q = {'name':q[:-4].replace('_', ' ').title(), 'len':len(data), 'q':[i.split(' ', 1) for i in data]}
 	QUOTES.append(q)
 
 @app.route('/')
 def root():
 	return render_template('index.html', quotes=QUOTES)
 
-@app.route('/quote/<qid>')
+@app.route('/<qid>')
 def quote(qid):
-	return render_template('quotes.html', quote=QUOTES[int(qid)], qlen=len(QUOTES), n=int(qid))
+	if not qid.isdigit(): return ""
+	return render_template('quote.html', quote=QUOTES[int(qid)], qlen=len(QUOTES), n=int(qid))
 
 if __name__ == "__main__":
     app.run(debug=True)
