3674e8e511d3ff2160fc836241ee0edd11b25689
B1naryth1ef <b1naryth1ef@gmail.com>
2012-07-17 01:53:54 -0500

    Moved boot stuff to a function

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ app = Flask(__name__)
 
 QUOTES = []
 
-for q in os.listdir('./quotes/'):
-	if not q.endswith('.txt'): continue
-	with open('./quotes/'+q) as f:
-		data = [i.strip() for i in f.readlines()]
-	q = {'name':q[:-4].replace('_', ' ').title(), 'len':len(data), 'q':[i.split(' ', 1) for i in data]}
-	QUOTES.append(q)
+def onBoot():
+	global QUOTES
+	for q in os.listdir('./quotes/'):
+		if not q.endswith('.txt'): continue
+		with open('./quotes/'+q) as f:
+			data = [i.strip() for i in f.readlines()]
+		q = {'name':q[:-4].replace('_', ' ').title(), 'len':len(data), 'q':[i.split(' ', 1) for i in data]}
+		QUOTES.append(q)
 
 @app.route('/')
 def root():
@@ -19,6 +21,8 @@ def root():
 def quote(qid):
 	if not qid.isdigit(): return ""
 	return render_template('quote.html', quote=QUOTES[int(qid)], qlen=len(QUOTES), n=int(qid))
+
+onBoot()
 
 if __name__ == "__main__":
     app.run()
